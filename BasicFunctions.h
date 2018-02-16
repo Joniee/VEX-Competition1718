dd7e1c2eecd6562628c33fccb5917fa956a57fca
JONATHAN <jondlmorga@gmail.com>
2018-02-16 22:14:59 +0100

    First steps for header fuctions.

---
+++ BasicFunctions.h
@@ -0,0 +1,31 @@
+//* Basic functions and global vars for move robot and their parts.
+
+int OFF = 0;
+int MAX = 250;
+int MIN = -250;
+
+
+
+
+
+
+void control();
+void arm();
+void movement();
+void allInOff();
+
+int percent(int value);
+
+void forwardMove();
+void backwardMove();
+void leftMove();
+void rightMove();
+
+void heightMove();
+
+void weightMove();
+
+void pinMove();
+
+void armMove()
+
